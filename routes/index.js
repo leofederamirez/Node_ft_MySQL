bb707d46023be920064626abb76266663b6ec719
leandro <eva.leandroramirez@gmail.com>
2021-01-27 22:43:25 -0300

    mas rutas y plantillas

--- routes/index.js
+++ routes/index.js
@@ -13,7 +13,10 @@ router.get('/create', (req,res) => {
 
 /* GET update page. */
 router.get('/update', (req,res) => {
-  res.render('update', {title: 'CRUD'})
+  res.render('update', {title: 'CRUD', legend: 'Actualizar Registro', label: 'Id', button: 'Actualizar'})
 })
-
+ /* GET delete page.*/
+ router.get('/delete', (req,res) => {
+   res.render('delete', {title: 'CRUD', legend: 'Eliminar Registro', label: 'Id', button: 'Eliminar'})
+ })
 module.exports = router;
