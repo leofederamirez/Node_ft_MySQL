8465fd5ee071c5956094eb8b9109c767ec30fcbf
leandro <eva.leandroramirez@gmail.com>
2021-01-27 22:10:47 -0300

    plantillas con EJS & rutas

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,15 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'CRUD' });
 });
 
+/* GET create page. */
+router.get('/create', (req,res) => {
+  res.render('create', { title: 'CRUD' })
+})
+
+/* GET update page. */
+router.get('/update', (res,req) => {
+  console.log(res)
+  req.render('update', {title: 'CRUD'})
+})
+
 module.exports = router;
